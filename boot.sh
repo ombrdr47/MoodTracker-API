f485e5fd4b5172d141872a7333b27e27a4ea69b6
ombrdr47 <152481302+ombrdr47@users.noreply.github.com>
2024-01-10 10:41:03 +0530

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
+
+# Activate virtual environment
 source venv/bin/activate
-flask db upgrade
-flask translate compile
-exec gunicorn -b :5000 --access-logfile - --error-logfile - mood_tracker:app
+
+# Check for errors in each command
+flask db upgrade || exit 1
+flask translate compile || exit 1
+
+# Start Gunicorn with explicit Python interpreter
+exec gunicorn -b :5000 --access-logfile - --error-logfile - "mood_tracker:app"
