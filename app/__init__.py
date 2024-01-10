760594bc6e6086331365f838fe294e8306b5a318
ombrdr47 <152481302+ombrdr47@users.noreply.github.com>
2024-01-10 10:27:46 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,12 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from config import Config
 
-
 app = Flask(__name__)
 app.config.from_object(Config)
 
-login = LoginManager(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+login = LoginManager(app)
 
 from app import routes, models
