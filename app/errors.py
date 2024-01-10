224737020470c9ab70aa910026926fc1ae94692f
ombrdr47 <152481302+ombrdr47@users.noreply.github.com>
2024-01-10 10:29:21 +0530

    Update errors.py

--- app/errors.py
+++ app/errors.py
@@ -1,38 +1,28 @@
-from flask import jsonify
+from flask import jsonify, current_app
 from app import app, db
 
+def generate_error_response(error, status_code):
+    return jsonify({"errors": {error.name: error.description}}), status_code
 
 @app.errorhandler(400)
 def handle_bad_request_error(error):
-    return jsonify({"errors": {error.name: error.description}}), 400
-
+    return generate_error_response(error, 400)
 
 @app.errorhandler(401)
 def handle_unauthorized_error(error):
-    return jsonify({"errors": {error.name: error.description}}), 401
-
+    return generate_error_response(error, 401)
 
 @app.errorhandler(404)
 def handle_not_found_error(error):
-    return jsonify({"errors": {error.name: error.description}}), 404
-
+    return generate_error_response(error, 404)
 
 @app.errorhandler(422)
 def handle_validation_error(error):
     exc = error.exc
     return jsonify({"errors": exc.messages}), 422
 
-
 @app.errorhandler(500)
 def handle_internal_error(error):
     db.session.rollback()
-    return (
-        jsonify(
-            {
-                "errors": {
-                    "An unexpected error has occurred that will likely require an administrator. Sorry for the inconvenience!"
-                }
-            }
-        ),
-        500,
-    )
+    current_app.logger.error("An unexpected error occurred:", exc_info=True)
+    return generate_error_response(error, 500)
